9eebe9ffae28c6a2f29b1b2c7f26295ba80c52c4
tohutohu <tohu.soy@gmail.com>
2019-11-22 00:11:54 +0900

    server: config関連の切り出し

--- server/router/router.go
+++ server/router/router.go
@@ -6,9 +6,7 @@ import (
 	"github.com/labstack/echo/v4"
 	"github.com/rakyll/statik/fs"
 	"github.com/ymotongpoo/goltsv"
-	"gopkg.in/yaml.v2"
 	"io/ioutil"
-	"main/config"
 	"main/config/manager"
 	"main/model"
 	"main/pubsub"
@@ -34,78 +32,11 @@ func (r *router) SetUp(e *echo.Echo) error {
 	e.GET("/api/ping", r.ping)
 	e.GET("/*", r.getStaticHandler())
 
-	e.GET("/api/log", func(c echo.Context) error {
-		//デモ用の実装
-		data, err := ioutil.ReadFile(`logFile`)
-		if err != nil {
-			pubsub.SystemEvent.Pub(pubsub.System{Time: time.Now(), Type: systemevent.ERROR, Message: err.Error()})
-		}
-		b := bytes.NewBufferString(string(data))
-		reader := goltsv.NewReader(b)
-		records, _ := reader.ReadAll()
-		bytes, _ := json.Marshal(records)
-		return c.JSON(200, string(bytes))
-	})
-	e.GET("/api/alp", func(c echo.Context) error {
-		out, err := exec.Command("sh", "-c", "cat logFile | alp --sort=max ltsv").Output()
-		if err != nil {
-			pubsub.SystemEvent.Pub(pubsub.System{Time: time.Now(), Type: systemevent.ERROR, Message: err.Error()})
-		}
-		return c.String(200, string(out))
-	})
-
-	e.GET("/api/config", func(c echo.Context) error {
-		config := r.configManager.Get()
-		buf, err := yaml.Marshal(config)
-		if err != nil {
-			return c.JSON(http.StatusInternalServerError, err)
-		}
-		return c.JSON(http.StatusOK, struct {
-			Yaml string
-		}{
-			Yaml: string(buf),
-		})
-	})
-
-	e.POST("/api/config", func(c echo.Context) error {
-		req := &struct {
-			Yaml string `json:"yaml"`
-		}{}
-		c.Bind(req)
-
-		newConfig := config.Config{}
-		if err := yaml.Unmarshal([]byte(req.Yaml), &newConfig); err != nil {
-			return c.JSON(http.StatusInternalServerError, err)
-		}
-		if err := r.configManager.SetConfig(newConfig); err != nil {
-			return c.JSON(http.StatusBadRequest, err)
-		}
-
-		return c.NoContent(http.StatusOK)
-	})
-
-	e.POST("/api/config/save", func(c echo.Context) error {
-		req := &struct {
-			Name string `json:"name"`
-			Yaml string `json:"yaml"`
-		}{}
-		c.Bind(req)
-
-		newConfig := config.Config{}
-		if err := yaml.Unmarshal([]byte(req.Yaml), &newConfig); err != nil {
-			return c.JSON(http.StatusInternalServerError, err)
-		}
-		if err := r.configManager.SetConfig(newConfig); err != nil {
-			return c.JSON(http.StatusBadRequest, err)
-		}
-
-		if err := r.configManager.Save(); err != nil {
-			return c.JSON(http.StatusInternalServerError, err)
-		}
-
-		return c.NoContent(http.StatusOK)
-	})
-
+	e.GET("/api/log", r.getLog)
+	e.GET("/api/alp", r.getALP)
+	e.GET("/api/config", r.getConfig)
+	e.POST("/api/config", r.postConfig)
+	e.POST("/api/config/save", r.postSaveConfig)
 	return nil
 }
 
@@ -122,3 +53,25 @@ func (r *router) getStaticHandler() func(c echo.Context) error {
 	h := http.FileServer(statikFs)
 	return echo.WrapHandler(http.StripPrefix("/", h))
 }
+
+func (r *router) getLog(c echo.Context) error {
+	//デモ用の実装
+	data, err := ioutil.ReadFile(`logFile`)
+	if err != nil {
+		pubsub.SystemEvent.Pub(pubsub.System{Time: time.Now(), Type: systemevent.ERROR, Message: err.Error()})
+	}
+	b := bytes.NewBufferString(string(data))
+	reader := goltsv.NewReader(b)
+	records, _ := reader.ReadAll()
+	bytes, _ := json.Marshal(records)
+	return c.JSON(200, string(bytes))
+}
+
+func(r *router) getALP (c echo.Context) error {
+	out, err := exec.Command("sh", "-c", "cat logFile | alp --sort=max ltsv").Output()
+	if err != nil {
+		pubsub.SystemEvent.Pub(pubsub.System{Time: time.Now(), Type: systemevent.ERROR, Message: err.Error()})
+	}
+	return c.String(200, string(out))
+}
+
