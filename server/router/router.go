ae670d69cad88f1a71cfd1e4055ea89aa285746c
Takamichi Omori <merrytakamerry@gmail.com>
2019-12-01 10:06:45 +0900

    ltsv2json を抹殺した alp は 現状使っていないので削除

--- server/router/router.go
+++ server/router/router.go
@@ -3,17 +3,16 @@ package router
 import (
 	"bytes"
 	"encoding/json"
-	"fmt"
-	"github.com/labstack/echo/v4"
-	"github.com/rakyll/statik/fs"
-	"github.com/ymotongpoo/goltsv"
 	"io/ioutil"
 	"main/manager"
 	"main/pubsub"
 	"main/pubsub/systemevent"
 	"net/http"
-	"os/exec"
 	"time"
+
+	"github.com/labstack/echo/v4"
+	"github.com/rakyll/statik/fs"
+	"github.com/ymotongpoo/goltsv"
 )
 
 type router struct {
@@ -33,10 +32,7 @@ func (r *router) SetUp(e *echo.Echo) error {
 
 	api.GET("/404", r.defaultBackend)
 	api.GET("/ping", r.ping)
-	api.GET("/log", r.getLog)
-	api.GET("/alp", r.getALP)
-	api.GET("/rawLog", r.getRawLogs)
-	api.GET("/accessLog", r.getAccessLog)
+	api.GET("/logs", r.getRawLogs)
 
 	api.GET("/config", r.getConfig)
 	api.POST("/config", r.postConfig)
@@ -63,45 +59,17 @@ func (r *router) getStaticHandler() func(c echo.Context) error {
 	return echo.WrapHandler(http.StripPrefix("/", h))
 }
 
-func (r *router) getLog(c echo.Context) error {
-	//デモ用の実装
-	data, err := ioutil.ReadFile(`logFile`)
+func (r *router) getRawLogs(c echo.Context) error {
+	data, err := ioutil.ReadFile(`./accessLog`)
 	if err != nil {
 		pubsub.SystemEvent.Pub(pubsub.System{Time: time.Now(), Type: systemevent.ERROR, Message: err.Error()})
 	}
 	b := bytes.NewBufferString(string(data))
+
 	reader := goltsv.NewReader(b)
 	records, _ := reader.ReadAll()
 	bytes, _ := json.Marshal(records)
-	return c.JSON(200, string(bytes))
-}
-
-func (r *router) getALP(c echo.Context) error {
-	out, err := exec.Command("sh", "-c", "cat accessLog | alp --sort=max ltsv").Output()
-	if err != nil {
-		pubsub.SystemEvent.Pub(pubsub.System{Time: time.Now(), Type: systemevent.ERROR, Message: err.Error()})
-	}
-	return c.String(200, string(out))
-}
-
-func (r *router) getRawLogs(c echo.Context) error {
-	// out, err := exec.Command("sh", "-c", "cat accessLog | ltsv2json | jq -c '[.[].host] | group_by(.) | map({(.[0]): length})'").Output()
-	// time と host のみ出す
-	out, err := exec.Command("sh", "-c", "cat accessLog | ltsv2json | jq -c '{time, host}' | jq -s").Output()
-	if err != nil {
-		pubsub.SystemEvent.Pub(pubsub.System{Time: time.Now(), Type: systemevent.ERROR, Message: err.Error()})
-	}
-	return c.String(200, string(out))
-}
-
-func (r *router) getAccessLog(c echo.Context) error {
-	out, err := exec.Command("sh", "-c", "cat accessLog | ltsv2json | jq -c '{uri, method, status}' | jq -s").Output()
-	fmt.Println(string(out))
-	fmt.Println(err)
-	if err != nil {
-		pubsub.SystemEvent.Pub(pubsub.System{Time: time.Now(), Type: systemevent.ERROR, Message: err.Error()})
-	}
-	return c.String(200, string(out))
+	return c.String(200, string(bytes))
 }
 
 func (r *router) defaultBackend(c echo.Context) error {
