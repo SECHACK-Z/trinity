23e8a652ffb05efd8ed8fec67312a9449647c89c
Takamichi Omori <merrytakamerry@gmail.com>
2019-12-08 17:17:12 +0900

    fix message

--- server/pubsub/systemevent/system_event.go
+++ server/pubsub/systemevent/system_event.go
@@ -10,11 +10,11 @@ const (
 	NEW_CONFIG_SAVE       SystemEventType = "new configuration file save"
 	HEALTH_CHECK_REGISTER SystemEventType = "new health check register"
 	CD_REGISTER           SystemEventType = "new Repository register for CD"
-	ERROR                 SystemEventType = "error"
 	APPLICATION_START     SystemEventType = "application start"
 	KILL_RECEIVED         SystemEventType = "received kill signal"
-	KILL_SUCCESS          SystemEventType = "successfully  killed application"
+	KILL_SUCCESS          SystemEventType = "successfully killed application"
 	KILL_FAILED           SystemEventType = "failed to kill application"
 	WEBHOOK_RECEIVED      SystemEventType = "webhook from github received"
 	REPOSITORY_UPDATED    SystemEventType = "respository is updated"
+	ERROR                 SystemEventType = "error"
 )
