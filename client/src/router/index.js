2fb3982cd08022d17b96fe22aa1b27349e1136e4
Takamichi Omori <merrytakamerry@gmail.com>
2020-01-22 18:10:39 +0900

    起動に関するドキュメント (#71)
    * initial commit
    
    * gettingStartedを書く
    
    * 起動
    
    * fix
    
    * add cd, healthcheck

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -155,6 +155,24 @@ export const constantRoutes = [
         component: () => import('@/views/documentation/index'),
         name: 'nest',
         meta: { title: 'Documentation', icon: 'documentation', affix: true, path: 'index' }
+      },
+      {
+        path: 'gettingStarted',
+        component: () => import('@/views/documentation/index'),
+        name: 'Getting started',
+        meta: { title: 'Getting Started', icon: 'documentation', affix: true, path: 'gettingStarted' }
+      },
+      {
+        path: 'healthCheck',
+        component: () => import('@/views/documentation/index'),
+        name: 'Health Check',
+        meta: { title: 'Health Check', icon: 'documentation', affix: true, path: 'healthCheck' }
+      },
+      {
+        path: 'continuousDelivery',
+        component: () => import('@/views/documentation/index'),
+        name: 'Continuous Delivery',
+        meta: { title: 'Continuous Delivery', icon: 'documentation', affix: true, path: 'continuousDelivery' }
       }
     ]
   },
